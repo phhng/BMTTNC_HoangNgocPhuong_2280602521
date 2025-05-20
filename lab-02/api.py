cbf009e3a9a1d509cda383431fc356a4d6e26b1d
phhng <hphuong11204@gmail.com>
2025-05-20 15:07:47 +0700

    [add] lab-02 railfence

--- lab-02/api.py
+++ lab-02/api.py
@@ -1,11 +1,29 @@
 from flask import Flask, request, jsonify
 from cipher.caesar import CaesarCipher
 from cipher.vigenere import VigenereCipher
+from cipher.railfence import RailFenceCipher
 
 app = Flask(__name__)
 
 caesar_cipher = CaesarCipher()
 vigenere_cipher = VigenereCipher()
+railfence_cipher = RailFenceCipher()
+
+@app.route("/api/railfence/encrypt", methods=["POST"])
+def railfence_encrypt():
+    data = request.json
+    plain_text = data['plain_text']
+    key = int(data['key'])
+    encrypted_text = railfence_cipher.rail_fence_encrypt(plain_text, key)
+    return jsonify({'encrypted_message': encrypted_text})
+
+@app.route("/api/railfence/decrypt", methods=["POST"])
+def railfence_decrypt():
+    data = request.json
+    cipher_text = data['cipher_text']
+    key = int(data['key'])
+    decrypted_text = railfence_cipher.rail_fence_decrypt(cipher_text, key)
+    return jsonify({'decrypted_message': decrypted_text})
 
 @app.route("/api/vigenere/encrypt", methods=["POST"])
 def vigenere_encrypt():
