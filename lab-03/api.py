91f1676394ab351587ccde3c8daa694108894803
Phuong <hphuong11204@gmail.com>
2025-06-03 16:22:30 +0700

    [add + update] lab-03 ecc init api

--- lab-03/api.py
+++ lab-03/api.py
@@ -1,8 +1,10 @@
 from flask import Flask, request,jsonify
 from cipher.rsa import RSACipher
+from cipher.ecc import ECCCipher
 
 app = Flask(__name__)
 rsa_cipher = RSACipher()
+ecc_cipher = ECCCipher()
 
 @app.route('/api/rsa/generate_keys', methods =['GET'])
 def rsa_generate_key():
@@ -59,6 +61,20 @@ def rsa_verify_signature():
     signature = bytes.fromhex(signature_hex)
     is_verified = rsa_cipher.verify(message,signature,public_key)
     return jsonify({'is_verified': is_verified})
+
+@app.route('/api/ecc/generate_keys', methods=['GET'])
+def ecc_generate_keys():
+    ecc_cipher.generate_keys()
+    return jsonify({'message': 'Key generated successfully'})
+
+@app.route('/api/ecc/sign', methods=['POST'])
+def ecc_sign_message():
+    data = request.json()
+    message = data['message']
+    private_key,_ = ecc_cipher.load_keys()
+    signature = ecc_cipher.sign(message,private_key)
+    signature_hex = signature.hex()
+    return jsonify({'signature': signature_hex})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
